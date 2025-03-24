4e1596fea2d67f1a52119f7ef8afca19734143f5
bek-ikov <asilbek.khalikovg@gmail.com>
2025-03-24 10:35:24 +0100

    Weapon System Good Progress

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,12 +6,29 @@ public class Gun : MonoBehaviour
 {
     [SerializeField] GunData gunData;
 
+    float timeSinceLastShot;
     public void Start()
     {
         PlayerShoot.shootInput += Shoot;
     }
+
+    private bool CanShoot() => !gunData.reloading && timeSinceLastShot > 1f / (gunData.fireRate / 60f);
     public void Shoot()
     {
-        Debug.Log("Gun Shot!");
+        if (gunData.currentAmmo > 0) 
+        { 
+            if (CanShoot())
+            {
+                if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo, gunData.maxDistance))
+                {
+                    Debug.Log(hitInfo.transform.name);
+                }
+
+                gunData.currentAmmo--;
+                timeSinceLastShot = 0;
+                OnGunShot();
+            }
+        }
     }
+    private void OnGunShot() { }
 }
