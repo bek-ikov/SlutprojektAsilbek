8b58a7b26e83d2e75e14db7942850208f93ec70b
bek-ikov <asilbek.khalikovg@gmail.com>
2025-05-16 04:34:26 +0200

    health counter

--- Assets/Scripts/HealthCounter.cs
+++ Assets/Scripts/HealthCounter.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthCounter : MonoBehaviour
 {
     public PlayerHealth playerHealth;
+    public Image heartImage;
+
+    public Sprite heartSprite;
+    public Sprite skullSprite;
 
     [SerializeField]
     private TextMeshProUGUI healthCounter;
@@ -14,10 +19,12 @@ public class HealthCounter : MonoBehaviour
     {
         if (playerHealth.currentHealth > 0)
         {
+            heartImage.sprite = heartSprite;
             healthCounter.text = $"{playerHealth.currentHealth}/{playerHealth.maxHealth}";
         }
         else
         {
+            heartImage.sprite = skullSprite;
             healthCounter.text = "Spelare död";
         }
     }
